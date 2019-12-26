fa6d72fd9654eea7d61db26177d12da255a8be30
Michal Nosavcov <mnosavcov@gmail.com>
2019-12-26 09:04:32 +0100

    resources

--- public/js/app.js
+++ public/js/app.js
@@ -49574,6 +49574,17 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/less/cms2020.less":
+/*!*************************************!*\
+  !*** ./resources/less/cms2020.less ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ "./resources/sass/app.scss":
 /*!*********************************!*\
   !*** ./resources/sass/app.scss ***!
@@ -49586,13 +49597,14 @@ __webpack_require__.r(__webpack_exports__);
 /***/ }),
 
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!*******************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/less/cms2020.less ./resources/sass/app.scss ***!
+  \*******************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! D:\vyvoj\nosavcov_michal\cms2020\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! D:\vyvoj\nosavcov_michal\cms2020\resources\less\cms2020.less */"./resources/less/cms2020.less");
 module.exports = __webpack_require__(/*! D:\vyvoj\nosavcov_michal\cms2020\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
